d0006c6aff358a3f969cb707d152790bea4d4d18
Iury Christimas Santos Oliveira <iury.oliveira-for@MVFOR.local>
2020-11-05 07:58:16 -0300

    first commit

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: `<h1>Angular com Typescript</h1>
+    <contatos-lista></contatos-lista>
+    `
+})
+export class AppComponent {
+
+}
